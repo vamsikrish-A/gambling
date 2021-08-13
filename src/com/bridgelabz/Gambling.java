0f2afcbde4c451f3eda79010ec99f16ab10d8b06
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-13 10:00:31 +0530

    added Win Or loose

--- src/com/bridgelabz/Gambling.java
+++ src/com/bridgelabz/Gambling.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
 public class Gambling {
+    public static final int STAKEPERDAY = 100;
+    public static final int BETPERGAME = 1;
+    public static final int WIN = 1;
+
+    public static void WINORLOOSE() {
+        double result = Math.floor(Math.random() * 10) % 2;
+         if (result == WIN)
+             System.out.println("You WON.");
+         else
+             System.out.println("you loose.");
+
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling simulator");
+        WINORLOOSE();
     }
 }
